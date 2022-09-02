89200f1cf643ed7d9417b340d15baf2760f05994
Пащенко Владимир <pikimelposhta@gmail.com>
2022-09-02 17:06:18 +0300

    'update'

--- src/js/card.js
+++ src/js/card.js
@@ -1,9 +1,9 @@
-const cardWrapper = document.querySelector(".card-wrapper");
 const cardElem = document.querySelector(".card");
 const cardButtons = document.querySelectorAll(".event-but");
 
 cardButtons[0].addEventListener("click", () => {
   minimizeCard();
+  setTimeout(loadPrevWord, 500);
 });
 
 cardButtons[1].addEventListener("click", () => {
@@ -12,6 +12,7 @@ cardButtons[1].addEventListener("click", () => {
 
 cardButtons[2].addEventListener("click", () => {
   minimizeCard();
+  setTimeout(loadNextWord, 500);
 });
 
 function minimizeCard() {
@@ -25,6 +26,32 @@ function turnCard() {
   cardElem.classList.toggle("rotate");
 }
 cardElem.addEventListener("click", (e) => {});
+
+function loadNextWord() {
+  selectedId.selectedWord++;
+  if (selectedId.selectedWord > listWords.length) selectedId.selectedWord = 0;
+
+  loadWordData(selectedId.selectedWord);
+}
+
+function loadPrevWord() {
+  selectedId.selectedWord--;
+  if (selectedId.selectedWord < -1) selectedId.selectedWord = -1;
+
+  loadWordData(selectedId.selectedWord);
+}
+
+function loadWordData(index) {
+  if (index >= 0 && index < listWords.length) {
+    let eng = listWords[index].eng;
+    let rus = listWords[index].rus;
+    cardElem.children[0].textContent = eng;
+    cardElem.children[1].textContent = rus;
+  } else if (index == -1) {
+    cardElem.children[0].textContent = "Start";
+    cardElem.children[1].textContent = "Начать";
+  }
+}
 
 // ===================================================== //
 
@@ -49,7 +76,7 @@ let listWords = [];
 const selectedId = {
   selectedCategory: categoryId[0],
   selectedModule: null,
-  selectedWord: 0,
+  selectedWord: null,
 };
 
 // ============================================ //
@@ -67,6 +94,19 @@ async function loadData() {
   renderModules();
 }
 
+async function loadWords(idModule) {
+  listWords = await DynamoAPI.getItems(
+    "flash-cards-words",
+    idModule,
+    "idModule"
+  );
+  if (listWords.length > 0) {
+    selectedId.selectedWord = -1;
+    cardElem.children[0].textContent = "Start";
+    cardElem.children[1].textContent = "Начать";
+  }
+}
+
 function renderModules() {
   refs.listModuleElement.innerHTML = listModuleTemplate(listModules);
 }
@@ -75,6 +115,17 @@ refs.listModuleElement.addEventListener("click", onModuleClick);
 // ============================================================ //
 
 function onModuleClick(e) {
-  if (e.target !== e.currentTarget) console.log(e.target.closest("li"));
+  if (e.target !== e.currentTarget) {
+    if (e.target.closest("li").dataset.id !== selectedId.selectedModule) {
+      let prevItem = document.querySelector(
+        ".list-module .list-module__item.select"
+      );
+      if (prevItem) prevItem.classList.remove("select");
+
+      e.target.classList.add("select");
+      selectedId.selectedModule = e.target.closest("li").dataset.id;
+      loadWords(selectedId.selectedModule);
+    }
+  }
 }
 // ============================================================ //
