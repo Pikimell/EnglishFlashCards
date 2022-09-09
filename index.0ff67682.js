0aef2b35e47a44bbc5887c6e2d1d71e55cd17f45
Pikimell <94864615+Pikimell@users.noreply.github.com>
2022-09-09 20:28:09 +0000

    Deploying to gh-pages from @ Pikimell/EnglishFlashCards@1f0a3cf4677701db491fbd93494abe761f54a0c8 🚀

---
+++ index.0ff67682.js
@@ -0,0 +1,2 @@
+const e=localStorage.getItem("learnModule1"),l=localStorage.getItem("learnModule2"),o=localStorage.getItem("learnedWords"),t=localStorage.getItem("savedWords");e||localStorage.setItem("learnModule1","[]"),l||localStorage.setItem("learnModule2","[]"),o||localStorage.setItem("learnedWords","[]"),t||localStorage.setItem("savedWords","[]");
+//# sourceMappingURL=index.0ff67682.js.map
---
+++ index.44452d2d.js
@@ -0,0 +1,2 @@
+!function(){const e=localStorage.getItem("learnModule1"),l=localStorage.getItem("learnModule2"),o=localStorage.getItem("learnedWords"),t=localStorage.getItem("savedWords");e||localStorage.setItem("learnModule1","[]"),l||localStorage.setItem("learnModule2","[]"),o||localStorage.setItem("learnedWords","[]"),t||localStorage.setItem("savedWords","[]")}();
+//# sourceMappingURL=index.44452d2d.js.map
